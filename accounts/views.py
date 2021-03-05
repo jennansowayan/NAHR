3abe194f00190aae17e44e36286ee3f9e15fc33c
Shahad Almugrin <almugrinshahad@gmail.com>
2021-03-05 17:40:35 +0300

    :wq some changes1:q!:wq

--- accounts/views.py
+++ accounts/views.py
@@ -33,10 +33,31 @@ def registerPage(request):
 
 def loginPage(request):
 
-  return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect!')
+        
+
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def home(request):
   return render(request, 'accounts/dashboard.html', {})
+
+def coursesPage(request):
+  return render(request, 'accounts/courses.html', {})
 
  
