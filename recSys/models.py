d4cb6e01cecdbcaba3a9136893b77cbf979f4d8a
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2021-03-08 18:59:51 +0300

    commit on main

--- recSys/models.py
+++ recSys/models.py
@@ -6,3 +6,11 @@ class User(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
 
+
+class Course(models.Model):
+    name = models.CharField(max_length = 50)
+    date = models.DateTimeField()
+    provider = models.CharField(max_length = 50)
+    p_link = models.CharField()
+    
+
