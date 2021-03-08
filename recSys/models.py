394006e98a6c14f0389108fcfa95ab1bf7b32cc1
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2021-03-08 19:45:51 +0300

    commit on main

--- recSys/models.py
+++ recSys/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
+    occupation = models.Choices()
 
 
 class Course(models.Model):
