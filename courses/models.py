3f21be56ed1cab5ee2c8f9d988132fac1bb1f747
Shahad Almugrin <almugrinshahad@gmail.com>
2021-04-05 18:10:14 +0300

    new commit

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Course(models.Model):
+class Technology(models.Model):
 	name = models.TextField(null=True,blank=True)
-	category = models.TextField(null=True,blank=True)
+	sub_category = models.TextField(null=True,blank=True)
+	description= models.TextField(null=True,blank=True)
+	fees= models.CharField(max_length=30)
+	link = models.URLField(max_length=200)
+
+class Business(models.Model):
+	name = models.TextField(null=True,blank=True)
+	sub_category = models.TextField(null=True,blank=True)
+	description= models.TextField(null=True,blank=True)
+	fees= models.CharField(max_length=30)
+	link = models.URLField(max_length=200)
+
+class Science(models.Model):
+	name = models.TextField(null=True,blank=True)
+	sub_category = models.TextField(null=True,blank=True)
+	description= models.TextField(null=True,blank=True)
+	fees= models.CharField(max_length=30)
+	link = models.URLField(max_length=200)
+
+class Art(models.Model):
+	name = models.TextField(null=True,blank=True)
+	sub_category = models.TextField(null=True,blank=True)
 	description= models.TextField(null=True,blank=True)
 	fees= models.CharField(max_length=30)
 	link = models.URLField(max_length=200)
