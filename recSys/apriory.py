0b7f5b6d0d30c3fd3eb177412b9ecac9730cd062
Shahad Almugrin <almugrinshahad@gmail.com>
2021-04-08 18:00:52 +0300

    -

--- recSys/apriory.py
+++ recSys/apriory.py
@@ -1,16 +1,18 @@
 from apyori import apriori
 import pandas as pd
 
-train_data = pd.read_csv( '/Users/jennansowayan/Nahr/NAHR/train_data.csv', header=None)
+train_data = pd.read_csv( '/Users/shahadalmugrin/Desktop/venv2/nahrv1/train_data.csv', delim_whitespace=True, header=None)
 data_len= len(train_data)
 
 
 records = []
 for i in range(0, data_len):
-    records.append([str(train_data.values[i,j]) for j in range(2)])
+    records.append([str(train_data.values[i,j]) for j in range(1)])
 print("hello")
-association_rules = apriori(records, min_support=0.001, min_confidence=0.2, min_lift=5, min_length=2)
+association_rules = apriori(records, min_support=0.00001, min_confidence=0.8, min_lift=2, min_length=0)
+
 association_results = list(association_rules)
+print(records)
 print(len(association_results))
 
 
