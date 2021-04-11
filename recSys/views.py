64c6901baf7492739899fb50c4d9198ca70fe201
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2021-04-11 18:51:36 +0300

    Revert "new quiz formMerge branch 'main' of https://github.com/jennansowayan/NAHR into main"
    This reverts commit c16595b65be0c12a343a8b6148d966761a4f6b45, reversing
    changes made to 4bed9335bbba1afbb038e7a0e1b4a64e11aa5e6c.

--- recSys/views.py
+++ recSys/views.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render
-from mlxtend.frequent_patterns import apriori, association_rules
-
+import apriory
 
 # Create your views here.
 
 
 class recommend(View):
-
-    train_data = pd.read_csv( '/Users/jennansowayan/Nahr/NAHR/train_data.csv', header=None)
-    data_len= len(train_data)
-
-
-    records = []
-    for i in range(0, data_len):
-        records.append([str(train_data.values[i,j]) for j in range(2)])
-
-    e = TransactionEncoder()
-    te_ary = te.fit(records).transform(records)
-    df = pd.DataFrame(te_ary, columns=te.columns_)
-    frequent_itemsets = apriori(df, min_support=0.01, use_colnames=True)
-
-    print(frequent_itemsets)
-
-
-    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.5)
-
-    for item in rules:
-        rule(antecedents = item["antecedents"], consequents = item["consequents"])
-
-
     def post(self, request):
         interests = request.POST.get()
         #store intrests
